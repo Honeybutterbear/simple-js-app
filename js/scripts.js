9395fe276d37b8b97bae4ee9a98d298ff9249ee8
Honeybutterbear <portee.lauren@gmail.com>
2021-12-05 17:14:11 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/*1.1 Excercise Code*/
+//Note: this is how you should notate .js code in short bursts//
+/* note: this is how you should notate code if you will be writing
+much longer content*/
+let myName = 'Lauren';
+document.write(myName);
+
+/*1.1 excercise: Math*/
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+//let size = 100;
+//let doubleSize = size * 2;
+//document.write(doubleSize);
+
+/*Note: can't include 'size' multiple times, the code below won't show if code
+above is active*/
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize)
